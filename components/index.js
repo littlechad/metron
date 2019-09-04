71fcd8666a04e158badbaf10f63ec9276711ab79
fian kurniawan <littlechad@matalenta.com>
2019-09-04 19:20:52 +0700

    [REMOVE] unnecessary components

--- components/index.js
+++ components/index.js
@@ -1,27 +1,13 @@
 import AlertComponent from 'components/Alert'
 import ContainerComponent from 'components/Container'
 import ContainerItemComponent from 'components/Container/Item'
-import CardComponent from 'components/Card'
-import CardActionsComponent from 'components/Card/Actions'
-import CardContentComponent from 'components/Card/Content'
-import CardHeaderComponent from 'components/Card/Header'
-import CardMediaComponent from 'components/Card/Media'
-import EmptyComponent from 'components/Empty'
 import HeadingComponent from 'components/Heading'
 import LoadingCircularComponent from 'components/Loading/Circular'
 import LoadingLinearComponent from 'components/Loading/Linear'
-import SearchComponent from 'components/Search'
 
 export const Alert = AlertComponent
 export const Container = ContainerComponent
 export const ContainerItem = ContainerItemComponent
-export const Card = CardComponent
-export const CardActions = CardActionsComponent
-export const CardContent = CardContentComponent
-export const CardHeader = CardHeaderComponent
-export const CardMedia = CardMediaComponent
-export const Empty = EmptyComponent
 export const Heading = HeadingComponent
 export const LoadingLinear = LoadingLinearComponent
 export const LoadingCircular = LoadingCircularComponent
-export const Search = SearchComponent
